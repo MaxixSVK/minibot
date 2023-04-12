6371ab1e08014c63394a8cd5ca60ebcbf2875ef7
Maxix <99895012+MaxixSVK@users.noreply.github.com>
2023-04-12 18:22:50 +0200

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,30 @@
+const { REST, Routes } = require("discord.js");
+const fs = require("node:fs");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const commands = [];
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        const data = await rest.put(
+            Routes.applicationCommands(process.env.clientId),
+            { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
