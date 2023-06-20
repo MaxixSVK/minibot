99f2b99c43f9937b294f2a8cf3e2de283f8ca0a6
Maxix <99895012+MaxixSVK@users.noreply.github.com>
2023-06-20 13:11:28 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,8 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`[INFO] Ready! Logged in as ${client.user.tag}`);
+        console.log(`[INFO] Successfully started bot`)
+        console.log(`[INFO] Logged in as ${client.user.tag}`);
         client.user.setPresence({
             activities: [{
                 name: config.ActivityName,
